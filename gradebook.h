a439ded6e2ad46482c6d09f9d97a30714f7bb53c
yash7210 <85038009+yash7210@users.noreply.github.com>
2022-10-27 19:23:29 -0400

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -3,17 +3,33 @@
 
 class Gradebook{
     private:
+//  Variables for the categorical grades
        double lab_grade;
        double assignment_grade;
        double term_project_grade;
+       double final_exam_grade;
+
+//  Variables for the final exam and final grade
+       double take_final_exam = false;
+       double possible_points;
+       double final_grade;
+
+//  Bonus points from review project
+       double extra_points;
+
     public:
        Gradebook();
+//  Function to read grades from the file and store into a vector
        Gradebook(std::string f_name, std::vector<std::vector<double>> *grades);
+    
+//  Functions to perform categorical and overall grade calculations
+    
        void calculate_lab_grade(std::vector<std::vector<double>> *grades);
        void calculate_assignment_grade(std::vector<std::vector<double>> *grades);
        void term_project(std::vector<std::vector<double>> *grades);
-       void final_exam(std::vector<std::vector<double>> *grades);
        void review_project(std::vector<std::vector<double>> *grades);
-       void overall_grade(std::vector<std::vector<double>> *grades);
+       void final_exam(std::vector<std::vector<double>> *grades);
+       void overall_grade();
+       void output_grades(std::vector<std::vector<double>> *grades);
 
 };
