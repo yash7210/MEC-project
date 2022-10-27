95052c17de78956711804e37e8f319f3df486427
yash7210 <85038009+yash7210@users.noreply.github.com>
2022-10-27 19:26:47 -0400

    Update gradesmain.cpp

--- gradesmain.cpp
+++ gradesmain.cpp
@@ -8,7 +8,30 @@ int main(int argc, char* argv[]){
     std::vector<std::vector<double>> grades;
 
     Gradebook g(f_name, &grades);
+
+//  Line breaks added to seperate functions in the output
+    std::cout << std::endl;
+
     g.calculate_lab_grade(&grades);
+//    std::cout << std::endl;
+
+    g.calculate_assignment_grade(&grades);
+//    std::cout << std::endl;
+
+    g.term_project(&grades);
+//    std::cout << std::endl;
+
+    g.review_project(&grades);
+//    std::cout << std::endl;
+
+    g.final_exam(&grades);
+//    std::cout << std::endl;
+
+    g.overall_grade();
+//    std::cout << std::endl;
+
+    g.output_grades(&grades);
+    std::cout << std::endl;
 
     return 0;
 }
